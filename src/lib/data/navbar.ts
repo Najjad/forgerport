ad14aa2862e2f90a01ba59e71dac04dc83456bbb
Najjad <najjadzeenni19@gmail.com>
2024-06-15 22:22:00 +0400

    Created contact page

--- src/lib/data/navbar.ts
+++ src/lib/data/navbar.ts
@@ -2,6 +2,7 @@ export const items = [
 	{ title: 'Skills', to: '/skills', icon: 'i-carbon-software-resource-cluster' },
 	{ title: 'Projects', to: '/projects', icon: 'i-carbon-cube' },
 	{ title: 'Experience', to: '/experience', icon: 'i-carbon-development' },
-	{ title: 'Education', to: '/education', icon: 'i-carbon-education' },
-	{ title: 'Resumé', to: '/resume', icon: 'i-carbon-result' }
+	{ title: 'Contact', to: '/contact', icon: 'i-carbon-email' } // New unique icon for Contact
+	//{ title: 'Education', to: '/education', icon: 'i-carbon-education' },
+	//{ title: 'Resumé', to: '/resume', icon: 'i-carbon-result' }
 ] as const;
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,72 @@
+<script>
+    import CommonPage from '$lib/components/CommonPage.svelte';
+    const email = "webforgerstudio@gmail.com"; 
+    const title = "Contact"; 
+</script>
+
+<CommonPage {title}>
+    <div class="contact-container">
+        <h1 class="contact-title">Would you like to get in touch?</h1>
+        <a href={`mailto:${email}`} class="contact-button">Contact Us</a>
+    </div>
+
+    <div class="contact-please"> 
+        <p class="contact-text">Contact us for any inquiries, </p>
+        <p class="contact-text">we can discuss your vision, pricing, and much more!</p>
+    </div>
+</CommonPage>
+
+<style lang="scss">
+    .contact-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: flex-start; 
+        padding-top: 50px; 
+        width: 100%;
+        background-color: #000; 
+        color: #fff;
+        font-family: Arial, sans-serif;
+        text-align: center;
+        height: auto; /* Changed from calc(100vh - 100px) to auto */
+    }
+
+    .contact-title {
+        font-size: 2em; /* Adjust the size as needed */
+    }
+
+    .contact-button {
+        background-color: #ff3e00; 
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 20px 0;
+        cursor: pointer;
+        border-radius: 4px;
+    }
+
+    .contact-button:hover {
+        background-color: #e33b00; 
+    }
+
+    .contact-please {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding-top: 20px; /* Add padding if needed */
+        color: #fff;
+        font-family: Arial, sans-serif;
+        text-align: center;
+        background-color: #000;
+    }
+
+    .contact-text {
+        font-size: 1.5em; /* Adjust the size as needed */
+        margin: 5px 0; /* Add some margin for spacing */
+    }
+</style>
